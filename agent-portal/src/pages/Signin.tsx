18e3e34c18e95d931698ba9b26721f09d8b53dd8
Dedan Wamalwa <wodedan000@gmail.com>
2023-10-23 00:22:45 +0300

    register and authenticate agent

--- agent-portal/src/pages/Signin.tsx
+++ agent-portal/src/pages/Signin.tsx
@@ -1,7 +1,55 @@
 import { Button, Label, Card } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Logo from "../components/content/Logo";
+import { useState } from "react";
 const SignIn = () => {
+    const location = useLocation();
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [loading, setLoading] = useState<boolean>();
+    const [error, setError] = useState<string | null>();
+    const navigate = useNavigate();
+
+    const submitDetails = async (): Promise<void> => {
+        setLoading(true);
+        if (!email || !password) {
+            setError("Please fill all the details");
+            setLoading(false);
+            return;
+        }
+        try {
+            const api = import.meta.env.VITE_PUBLIC_API_HOST;
+            const response = await fetch(`${api}/api/users/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                }),
+            });
+            if (response.status === 401) {
+                setError("Invalid email or password");
+                setLoading(false);
+                return;
+            }
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            navigate("/");
+        } catch (error: any | unknown) {
+            console.error("Error:", error.message);
+            setLoading(false);
+            setError("Sorry, an error occurred, please try again");
+            return;
+        }
+    };
     return (
         <div className="p-2 lg:p-0 flex items-center justify-center lg:h-screen lg:bg-gray-200">
             <Card className="md:w-2/3 lg:w-1/3">
@@ -18,7 +66,9 @@ const SignIn = () => {
                             id="email"
                             required
                             type="email"
-                            placeholder="name@aspire.com"
+                            placeholder="name@branch.com"
+                            value={location.state}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div>
@@ -30,9 +80,15 @@ const SignIn = () => {
                             id="password"
                             required
                             type="password"
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    <Button color="success" type="submit">
+                    {error && (
+                        <div>
+                            <div className="mb-2 block text-sm text-red-400">{error}</div>
+                        </div>
+                    )}
+                    <Button color="success" type="submit" onClick={submitDetails} isProcessing={loading} disabled={loading}>
                         Sign In
                     </Button>
                     <div>
