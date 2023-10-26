b4a1f0710ff29b55bdba1aa9bfe2b9b2d34808f1
Dedan Wamalwa <wodedan000@gmail.com>
2023-10-26 23:53:24 +0300

    Add Search functionality (#19)
    * add search functionality
    
    * add search functionality and improve UI

--- agent-portal/src/layouts/RootLayout.tsx
+++ agent-portal/src/layouts/RootLayout.tsx
@@ -7,7 +7,7 @@ const RootLayout = () => {
                 <div className="h-screen">
                     <SideBar />
                 </div>
-                <div className="py-2 px-6">
+                <div className="py-2 px-6 w-full md:w-3/4 h-screen">
                     <Outlet />
                 </div>
             </div>
